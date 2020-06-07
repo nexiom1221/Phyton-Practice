f5373d5603b785e348a808aaf1ea738ce45a9223
OJH <superdizmon1@gmail.com>
2020-06-07 21:37:53 +0900

    변수,함수,조건문

---
+++ 한줄for.py
@@ -0,0 +1,15 @@
+# students = [1,2,3,4,5]
+# print(students)
+
+# students = [i +100 for i in students]
+# print(students)
+
+
+# students = ["iron man", "thor", "i am groot"]
+# students = [len(i) for i in students]
+# print(students)
+
+students = ["iron man", "thor", "i am groot"]
+students = [i.upper() for i in students]
+
+print(students)
---
+++ 함수2.py
@@ -0,0 +1,6 @@
+def profile(name, age, main_lang):
+    print("이름 : {0}\t나이: {1}\t주 사용 언어: {2}" \
+            .format(name,age,main_lang))
+profile(name="유재석", main_lang="파이썬",age=20)
+profile(name="김태호", main_lang="자바",age=21)
+
