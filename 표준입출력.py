90c01eae638ad1c5ee168178a18d8d23813c3656
OJH <superdizmon1@gmail.com>
2020-06-15 23:25:23 +0900

    표준입출력

--- 표준입출력.py
+++ 표준입출력.py
@@ -12,10 +12,10 @@ print("무엇이 더 재미있을까요?")
 scores = {"수학":0, "영어":50, "코딩":100}
 for subject, score in scores.items():
     # print(subject, score)
-    print(subject.ljust(8), str(score).rjust(4), sep=":")
+    print(subject.ljust(3), str(score).rjust(4), sep=":")
 
 for num in range(1, 21):
-    print("대기번호 :" + str(num).zfill(3))
+    print("대기번호 :" + str(num).zfill(4))
 
 
 answer = input("아무 값이나 입력하세요 : ")
