0d5408f951237886edf4f52f0f022e2987190ac4
OJH <superdizmon1@gmail.com>
2020-08-26 22:23:26 +0900

    파일 정리

--- continue,break.py
+++
@@ -1,9 +0,0 @@
-absent = [2,5]
-no_book = [7]
-for student in range(1,11):
-    if student in absent:
-        continue
-    elif student in no_book:
-        print("오늘 수업 여기까지. {0}는 교무실로.".format(no_book))
-        break
-    print("{0}, 책을 읽어봐.".format(student))
