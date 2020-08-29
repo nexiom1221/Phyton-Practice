117c845af800ee6c2f7e621dfbf873a1181c403f
OJH <superdizmon1@gmail.com>
2020-08-29 18:24:13 +0900

    내부 모듈

---
+++ 모듈/math.py
@@ -0,0 +1,12 @@
+import math
+
+print(math.sin(1))
+print()
+print(math.tan(1))
+print()
+print(math.floor(2.5))
+print()
+print(math.ceil(2.5))
+print()
+print(math.floor(math.tan(1)))
+
---
+++ 모듈/math2.py
@@ -0,0 +1,11 @@
+from math import *
+
+print(sin(1))
+print()
+print(tan(1))
+print()
+print(floor(2.5))
+print()
+print(ceil(2.5))
+print()
+print(floor(tan(1)))
